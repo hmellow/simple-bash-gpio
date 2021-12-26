e7824b99a06ffda8adcd5673707fc4d572dff079
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-12-25 23:57:43 -0600

    GPIO declare & set

--- main.sh
+++ main.sh
@@ -1,8 +1,24 @@
 # Make a GPIO pin available as either an input or an output
-# Argument 1: "in" or "out"
-# Argument 2: Desired GPIO pin # (not physical #)
+# Argument 1: Desired GPIO pin # (not physical pin #)
+# Argument 2: "in" or "out"
 function gpioDec() {
-    local wdir =  ~/sys/class/gpio
-    echo $2 > wdir/export
-    echo $1 > wdir/gpio${$2}direction
+    local wdir=~/sys/class/gpio;
+    echo $1 > wdir/export;
+    echo $2 > wdir/gpio${$1}direction;
+}
+
+# Set an output GPIO pin to either HIGH or LOW
+# Argument 1: Desired GPIO pin # (not physical pin #)
+# Argument 2: "high" or "low"
+function gpioSet() {
+    local upperc=$2 | tr a-z A-Z;
+    if [ $upperc -e "HIGH"  ];
+    then
+        local state=1;
+    elif [ $upperc -e "LOW" ];
+    then
+        local state=0;
+    fi;
+
+    echo $2 > ~/sys/class/gpio/gpio${$1}/value;
 }
