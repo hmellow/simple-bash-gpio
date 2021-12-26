89a96fd030f33dcf7c68f118ed55a15cd595c3c5
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-12-25 23:34:18 -0600

    Specify pin to direct

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 # Make a GPIO pin available as either an input or an output
 # Argument 1: "in" or "out"
-# Argument 2: Desired GPIO pin #  (not physical #)
-function gpioSet() {
+# Argument 2: Desired GPIO pin # (not physical #)
+function gpioDec() {
     local wdir =  ~/sys/class/gpio
     echo $2 > wdir/export
-    echo $1 > wdir/direction
+    echo $1 > wdir/gpio${$2}direction
 }
