dd37af33fda9016e6ff2eb775853a4555af809d3
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-12-25 23:30:32 -0600

    Delete main.sh

--- main/main.sh
+++
@@ -1,9 +0,0 @@
-# Make a GPIO pin available as either an input or an output
-# Argument 1: "in" or "out"
-# Argument 2: Desired GPIO pin #  (not physical #)
-function gpioSet() {
-    local wdir =  ~/sys/class/gpio
-    echo $2 > wdir/export
-    echo $1 > wdir/direction
-}
-
